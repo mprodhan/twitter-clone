b8f3976abec03c6d845b60c1fed1acb35153455a
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-06-05 00:55:17 -0400

    worked on views.py and still debugging the file.

--- notification/urls.py
+++ notification/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from notification import views
 
-urlpatterns = []
+urlpatterns = [
+    path("visibletweet/<int:id>", views.visible_tweet),
+    path("invisibletweet/<int:id>", views.invisible_tweet)
+]
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render, reverse
 
-# Create your views here.
+from notification.models import Notification
+from tweet.models import Tweet
+from twitteruser.models import TwitterUser
+from tweet.views import tweetadd
+
+def visible_tweet(request, id):
+    notif = Notification.objects.get(id=id)
+    notif.message_visibility = False
+    return HttpResponseRedirect(reverse("homepage"), kwargs={"id": notif.target_user.id})
+
+def invisible_tweet(request, id):
+    notif = Notification.objects.get(id=id)
+    notif.message_visibility = True
+    return HttpResponseRedirect(reverse("homepage"), kwargs={"id": notif.target_user.id})
